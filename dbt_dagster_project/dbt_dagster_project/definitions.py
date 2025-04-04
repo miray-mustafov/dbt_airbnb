0da25ec752683b5895a4ef01ffe068f1c310cfa1
Miray Mustafov <mustafovmiray@gmail.com>
2025-04-04 10:58:07 +0300

    feat(orchestration): add dagster

---
+++ dbt_dagster_project/dbt_dagster_project/definitions.py
@@ -0,0 +1,16 @@
+from dagster import Definitions
+from dagster_dbt import DbtCliResource
+from .assets import dbtlearn_dbt_assets
+from .project import dbtlearn_project
+from .schedules import schedules
+
+defs = Definitions(
+    assets=[dbtlearn_dbt_assets],
+    schedules=schedules,
+    resources={
+        "dbt": DbtCliResource(
+            project_dir=dbtlearn_project,
+            # profiles_dir=path/to/profiles  currently dbt pro files.yml is found by default in the dbt project folder
+            ),
+    },
+)
